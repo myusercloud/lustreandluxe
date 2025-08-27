c3b3dfad53f3d231d9e36a378b668f398ea90936
Harrizon|Myusercloud <lucasharrizon@gmail.com>
2025-08-27 12:47:17 +0300

    create the product listing page

--- lustreandluxe/lustreandluxe/lustreandluxe/settings.py
+++ lustreandluxe/lustreandluxe/lustreandluxe/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files (Uploaded by users)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
