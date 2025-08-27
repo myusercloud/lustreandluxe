7f57ddb4515e3da00e7c093303ac9bd834fc8223
Harrizon|Myusercloud <lucasharrizon@gmail.com>
2025-08-27 12:21:17 +0300

    register models in admin interface

--- lustreandluxe/lustreandluxe/shop/admin.py
+++ lustreandluxe/lustreandluxe/shop/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Category, Product, Order, OrderItem
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "stock", "category", "created_at")
+    list_filter = ("category",)
+    search_fields = ("name", "description")
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer_name", "email", "phone", "created_at")
+    inlines = [OrderItemInline]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "order", "product", "quantity")
