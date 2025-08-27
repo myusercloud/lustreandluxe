9644c560db8fd83ad2f54380acee38206544fc61
Harrizon|Myusercloud <lucasharrizon@gmail.com>
2025-08-27 18:26:21 +0300

    Enhance product detail page layout and styling; improve cart functionality with type safety for product IDs

--- lustreandluxe/lustreandluxe/shop/views.py
+++ lustreandluxe/lustreandluxe/shop/views.py
@@ -1,6 +1,8 @@
 from pyexpat.errors import messages
+from django.contrib import messages
 from django.shortcuts import redirect, render, get_object_or_404
 from .models import Product
+from django.views.decorators.http import require_POST
 
 
 # Product views
@@ -19,7 +21,7 @@ def cart_detail(request):
     total = 0
 
     for product_id, quantity in cart.items():
-        product = get_object_or_404(Product, pk=product_id)
+        product = get_object_or_404(Product, pk=int(product_id))
         subtotal = product.price * quantity
         total += subtotal
         products.append({
@@ -31,9 +33,12 @@ def cart_detail(request):
     return render(request, 'shop/cart_detail.html', {'cart_products': products, 'total': total})
 
 
+@require_POST
 def cart_add(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)  # ensure product exists first
     cart = request.session.get('cart', {})
     quantity = int(request.POST.get('quantity', 1))
+
     cart[str(product_id)] = cart.get(str(product_id), 0) + quantity
     request.session['cart'] = cart
     return redirect('cart_detail')
