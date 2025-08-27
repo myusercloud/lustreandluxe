d220f7be7cc6876dc039830b0d19e7c6d99a7268
Harrizon|Myusercloud <lucasharrizon@gmail.com>
2025-08-27 13:04:07 +0300

    cart functionality

--- lustreandluxe/lustreandluxe/shop/urls.py
+++ lustreandluxe/lustreandluxe/shop/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>/', views.cart_add, name='cart_add'),
+    path('cart/remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
 ]
--- lustreandluxe/lustreandluxe/shop/views.py
+++ lustreandluxe/lustreandluxe/shop/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Product
 
+
+# Product views
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'shop/product_list.html', {'products': products})
@@ -8,3 +10,38 @@ def product_list(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'shop/product_detail.html', {'product': product})
+
+# Cart views
+def cart_detail(request):
+    cart = request.session.get('cart', {})
+    products = []
+    total = 0
+
+    for product_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=product_id)
+        subtotal = product.price * quantity
+        total += subtotal
+        products.append({
+            'product': product,
+            'quantity': quantity,
+            'subtotal': subtotal
+        })
+
+    return render(request, 'shop/cart_detail.html', {'cart_products': products, 'total': total})
+
+
+def cart_add(request, product_id):
+    cart = request.session.get('cart', {})
+    quantity = int(request.POST.get('quantity', 1))
+    cart[str(product_id)] = cart.get(str(product_id), 0) + quantity
+    request.session['cart'] = cart
+    return redirect('cart_detail')
+
+
+
+def cart_remove(request, product_id):
+    cart = request.session.get('cart', {})
+    if str(product_id) in cart:
+        del cart[str(product_id)]
+    request.session['cart'] = cart
+    return redirect('cart_detail')
